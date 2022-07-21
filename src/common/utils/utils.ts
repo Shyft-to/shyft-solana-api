88fda7340e90460976364946787be58166796e89
Ishkaran Singh <ishkaran@psychostore.in>
2022-07-22 00:17:35 +0530

    Using quicknode for account apis

--- src/common/utils/utils.ts
+++ src/common/utils/utils.ts
@@ -1,4 +1,19 @@
+import { clusterApiUrl } from '@solana/web3.js';
 import axios from 'axios';
+import { Network } from 'src/dto/netwotk.dto';
+
+const endpoint = {
+  http: {
+    devnet: 'http://api.devnet.solana.com',
+    testnet: 'http://api.testnet.solana.com',
+    'mainnet-beta': 'http://api.mainnet-beta.solana.com/'
+  },
+  https: {
+    devnet: 'https://broken-polished-dew.solana-devnet.discover.quiknode.pro/44ee984c6efc5b1e98c12010b70bb2342ab19c7d/',
+    testnet: 'https://api.testnet.solana.com',
+    'mainnet-beta': 'https://broken-hidden-pond.solana-mainnet.discover.quiknode.pro/a9443ebc1be769c64ae5364f8dc72762d37461cd/',
+  }
+};
 
 export const Utility = {
   request: async function (uri: string): Promise<any> {
@@ -9,4 +24,20 @@ export const Utility = {
       throw error;
     }
   },
+
+  clusterUrl: function (network: Network): string {
+    try {
+      switch (network) {
+        case Network.devnet:
+          return endpoint.https.devnet;
+        case Network.mainnet:
+          return endpoint.https['mainnet-beta'];
+        default:
+          return clusterApiUrl(network);
+      }
+    } catch (error) {
+      throw error;
+    }
+  },
+
 };
--- src/modules/account/account.service.ts
+++ src/modules/account/account.service.ts
@@ -15,7 +15,12 @@ import { TokenBalanceCheckDto } from './dto/token-balance-check.dto';
 import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import { RemoteDataFetcherService } from '../db/remote-data-fetcher/data-fetcher.service';
 import { FetchAllNftDto } from '../db/remote-data-fetcher/dto/data-fetcher.dto';
-import { getAllDomains, performReverseLookup, performReverseLookupBatch } from '@bonfida/spl-name-service';
+import {
+  getAllDomains,
+  performReverseLookup,
+  performReverseLookupBatch,
+} from '@bonfida/spl-name-service';
+import { Utility } from 'src/common/utils/utils';
 
 @Injectable()
 export class WalletService {
@@ -23,7 +28,10 @@ export class WalletService {
   async getBalance(balanceCheckDto: BalanceCheckDto): Promise<number> {
     try {
       const { wallet, network } = balanceCheckDto;
-      const connection = new Connection(clusterApiUrl(network), 'confirmed');
+      const connection = new Connection(
+        Utility.clusterUrl(network),
+        'confirmed',
+      );
       const balance = await connection.getBalance(new PublicKey(wallet));
       return balance / LAMPORTS_PER_SOL;
     } catch (error) {
@@ -34,7 +42,7 @@ export class WalletService {
   async getTokenBalance(balanceCheckDto: TokenBalanceCheckDto): Promise<number> {
     try {
       const { wallet, network, token } = balanceCheckDto;
-      const connection = new Connection(clusterApiUrl(network), 'confirmed');
+      const connection = new Connection(Utility.clusterUrl(network), 'confirmed');
       let tokenAccount;
       try {
         tokenAccount = await connection.getParsedTokenAccountsByOwner(
@@ -55,7 +63,7 @@ export class WalletService {
   async getAllTokensBalance(balanceCheckDto: BalanceCheckDto): Promise<Record<string, number>[]> {
     try {
       const { wallet, network } = balanceCheckDto;
-      const connection = new Connection(clusterApiUrl(network), 'confirmed');
+      const connection = new Connection( Utility.clusterUrl(network), 'confirmed');
       const allTokenInfo = [];
       try {
         const parsedSplAccts = await connection.getParsedTokenAccountsByOwner(new PublicKey(wallet), {programId: TOKEN_PROGRAM_ID});
@@ -107,7 +115,7 @@ export class WalletService {
   }
 
   async getDomains(walletDto: BalanceCheckDto) {
-    const connection = new Connection(clusterApiUrl(walletDto.network), 'confirmed');
+    const connection = new Connection(Utility.clusterUrl(walletDto.network), 'confirmed');
     const domains = await getAllDomains(connection, new PublicKey(walletDto.wallet));
 
     const names = await performReverseLookupBatch(connection, domains);
@@ -121,9 +129,8 @@ export class WalletService {
 
   async resolveAddress(nameAddressDto: ResolveAddressDto) {
     try {
-      const connection = new Connection(clusterApiUrl(nameAddressDto.network), 'confirmed');
+      const connection = new Connection(Utility.clusterUrl(nameAddressDto.network), 'confirmed');
       const name = await performReverseLookup(connection, new PublicKey(nameAddressDto.address));
-      console.log(name);
       return { name: `${name}.sol` };
     } catch (error) {
       throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -133,7 +140,7 @@ export class WalletService {
   async sendSol(sendSolDto: SendSolDto): Promise<string> {
     try {
       const { network, from_private_key, to_address, amount } = sendSolDto;
-      const connection = new Connection(clusterApiUrl(network), 'confirmed');
+      const connection = new Connection(Utility.clusterUrl(network), 'confirmed');
 
       const from = AccountUtils.getKeypair(from_private_key);
       // Add transfer instruction to transaction
