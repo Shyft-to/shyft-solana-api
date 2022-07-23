06020db5d199ffba85a9020cde296d7b1757a172
Ishkaran Singh <ishkaran@psychostore.in>
2022-07-23 14:39:59 +0530

    Adding quick node private nodes for devnet and mainnet beta

--- src/common/utils/utils.ts
+++ src/common/utils/utils.ts
@@ -1,6 +1,7 @@
 import { clusterApiUrl } from '@solana/web3.js';
 import axios from 'axios';
 import { Network } from 'src/dto/netwotk.dto';
+import { configuration } from '../configs/config';
 
 const endpoint = {
   http: {
@@ -9,9 +10,9 @@ const endpoint = {
     'mainnet-beta': 'http://api.mainnet-beta.solana.com/'
   },
   https: {
-    devnet: 'https://broken-polished-dew.solana-devnet.discover.quiknode.pro/44ee984c6efc5b1e98c12010b70bb2342ab19c7d/',
+    devnet: configuration().solDevnet,
     testnet: 'https://api.testnet.solana.com',
-    'mainnet-beta': 'https://broken-hidden-pond.solana-mainnet.discover.quiknode.pro/a9443ebc1be769c64ae5364f8dc72762d37461cd/',
+    'mainnet-beta': configuration().solMainnetBeta,
   }
 };
 
